be74ed45bdbda15c3761eba49f2b10b4a3045fcf
cooltoast <mpatel678@gmail.com>
2018-10-04 01:32:18 -0700

    gh-pages test

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Left4right::Application.config.secret_token = '46d9f3916302ffcbfd26e9f272d890a4e3f2deae41ad7fb0eecf1e9841ecdf9f8485e06180f4d929d70f9d520d5e1a80a68a57f75f8a1e7ed97e87d4b06e1d1d'
--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Left4right::Application.config.session_store :cookie_store, key: '_left4right_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Left4right::Application.config.session_store :active_record_store
